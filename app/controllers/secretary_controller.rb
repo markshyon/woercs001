774ae98a9e77d5f5b17e486fb876173a39e10c34
mark <woremark@gmail.com>
2018-12-05 16:54:30 +0800

    debug

--- app/controllers/secretary_controller.rb
+++ app/controllers/secretary_controller.rb
@@ -34,7 +34,7 @@ class SecretaryController < ApplicationController
     # 傳送訊息到Line
     def reply_to_line(reply_text)
         return nil if reply_text.nil?
-        
+
         # 取得reply token
         reply_token = params['events'][0]['replyToken']
 
@@ -55,4 +55,52 @@ class SecretaryController < ApplicationController
             config.channel_token = 'PJ13Or13E0TR+dy4CWR30jvA0dMNEqdRqtoB69YIrE7c8AAS0iqVZKgsxfifW4WJbzQlGZGXFKd7fekupwKWE7MikJ+fmy+sZW/30M/KOZJaP83FNSKwWSS1qforSoOhzs5cb2yKG+t1x6MduBJzLwdB04t89/1O/w1cDnyilFU='
         }
     end
+
+    def eat
+        render plain: "吃土啦"
+    end 
+    
+    def request_headers
+        render plain: request.headers.to_h.reject{ |key, value|
+          key.include? '.'
+        }.map{ |key, value|
+          "#{key}: #{value}"
+        }.sort.join("\n")
+    end
+    
+    def response_headers
+        response.headers['5566'] = 'QQ'
+        render plain: response.headers.to_h.map{ |key, value|
+          "#{key}: #{value}"
+        }.sort.join("\n")
+    end
+    
+    def request_body
+        render plain: request.body
+    end
+    
+    def show_response_body
+        puts "===這是設定前的response.body:#{response.body}==="
+        render plain: "虎哇花哈哈哈"
+        puts "===這是設定後的response.body:#{response.body}==="
+    end
+    
+    def sent_request
+        uri = URI('http://localhost:3000/kamigo/eat')
+        http = Net::HTTP.new(uri.host, uri.port)
+        http_request = Net::HTTP::Get.new(uri)
+        http_response = http.request(http_request)
+    
+        render plain: JSON.pretty_generate({
+          request_class: request.class,
+          response_class: response.class,
+          http_request_class: http_request.class,
+          http_response_class: http_response.class
+        })
+    end
+    
+    def translate_to_korean(message)
+        "#{message}油~"
+    end
+    
 end
