bf336dfaaf59e0629e7495fe53f2097c6c61b899
mark <woremark@gmail.com>
2018-12-05 16:47:41 +0800

    加入關鍵字回應功能

--- app/controllers/secretary_controller.rb
+++ app/controllers/secretary_controller.rb
@@ -2,60 +2,57 @@ require 'line/bot'
 class SecretaryController < ApplicationController
     protect_from_forgery with: :null_session
 
-    def eat
-        render plain: "吃吃吃"
-    end
-
-    def request_headers
-        render plain: request.headers.to_h.reject{
-            |key, value| key.include? '.'
-        }
-        .map{
-            |key, value| "#{key}: #{value}"
-        }.sort.join("\n")
-    end
-
-    def request_body
-        render plain: request.body
-    end
-
-    def response_headers
-        render plain: response.headers.to_h.map{
-            |key, value| "#{key}: #{value}"
-    }.sort.join("\n")
-    end
-    
-    def show_response_body
-        puts "小秘書在這為您服務"
-        render plain: response.body
-    end
-
-    def sent_request
-        uri = URI('http://localhost:3000/secretary/response_body')
-        response = Net::HTTP.get(uri)
-        render plain: response
-    end
-
     def webhook
-        # Line Bot API 物件初始化
-        client = Line::Bot::Client.new { |config|
-            config.channel_secret = '10f88109463f23cdf580ecddc364664c'
-            config.channel_token = 'PJ13Or13E0TR+dy4CWR30jvA0dMNEqdRqtoB69YIrE7c8AAS0iqVZKgsxfifW4WJbzQlGZGXFKd7fekupwKWE7MikJ+fmy+sZW/30M/KOZJaP83FNSKwWSS1qforSoOhzs5cb2yKG+t1x6MduBJzLwdB04t89/1O/w1cDnyilFU='
+        # 設定回覆訊息
+        reply_text = keyword_reply(received_text)
+
+        # 傳送訊息到Line
+        response = reply_to_line(reply_text)
+    
+        # 回應 200
+        head :ok
+    end
+
+    # 取得對方說的話
+    def received_text
+        message = params['events'][0]['message']['text']
+        message['text'] unless message.nil?
+    end
+
+    # 關鍵字回覆
+    def keyword_reply(received_text)
+        # 學習紀錄表
+        keyword_mapping = {
+            'QQ' => '123',
+            'ABC' => '456'
         }
-  
-        # 取得 reply token
+
+        # 查表
+        keyword_mapping[received_text]
+    end
+
+    # 傳送訊息到Line
+    def reply_to_line(reply_text)
+        return nil if reply_text.nil?
+        
+        # 取得reply token
         reply_token = params['events'][0]['replyToken']
 
         # 設定回覆訊息
         message = {
             type: 'text',
-            text: '好哦～好哦～'
+            text: reply_text
         }
 
         # 傳送訊息
-        response = client.reply_message(reply_token, message)
-    
-        # 回應 200
-        head :ok
+        line.reply_message(reply_token, message)
+    end
+
+    # Line Bot API 物件初始化
+    def line
+        @line ||= Line::Bot::Client.new { |config|
+            config.channel_secret = '10f88109463f23cdf580ecddc364664c'
+            config.channel_token = 'PJ13Or13E0TR+dy4CWR30jvA0dMNEqdRqtoB69YIrE7c8AAS0iqVZKgsxfifW4WJbzQlGZGXFKd7fekupwKWE7MikJ+fmy+sZW/30M/KOZJaP83FNSKwWSS1qforSoOhzs5cb2yKG+t1x6MduBJzLwdB04t89/1O/w1cDnyilFU='
+        }
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'secretary/eat', to: 'secretary#eat'
-  get 'secretary/request_headers', to: 'secretary#request_headers'
-  get 'secretary/request_body', to: 'secretary#request_body'
-  get 'secretary/response_headers', to: 'secretary#response_headers'
-  get 'secretary/response_body', to: 'secretary#show_response_body'
-  get 'secretary/sent_request', to: 'secretary#sent_request'
   post 'secretary/webhook', to: 'secretary#webhook'
 end
