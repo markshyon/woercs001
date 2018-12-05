2dbf000547f4ca94e98ceacf03b356958dcf48a5
mark <woremark@gmail.com>
2018-12-05 10:14:42 +0800

    init

---
+++ app/controllers/secretary_controller.rb
@@ -0,0 +1,36 @@
+class SecretaryController < ApplicationController
+    def eat
+        render plain: "吃吃吃"
+    end
+
+    def request_headers
+        render plain: request.headers.to_h.reject{
+            |key, value| key.include? '.'
+        }
+        .map{
+            |key, value| "#{key}: #{value}"
+        }.sort.join("\n")
+    end
+
+    def request_body
+        render plain: request.body
+    end
+
+    def response_headers
+        render plain: response.headers.to_h.map{
+            |key, value| "#{key}: #{value}"
+    }.sort.join("\n")
+    end
+    
+    def show_response_body
+        puts "小秘書在這為您服務"
+        render plain: response.body
+    end
+
+    def sent_request
+        uri = URI('http://localhost:3000/secretary/response_body')
+        response = Net::HTTP.get(uri)
+        render plain: response
+    end
+
+end
---
+++ test/controllers/secretary_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SecretaryControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
